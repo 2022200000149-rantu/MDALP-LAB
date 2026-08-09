8816ca275f060dd3055018ee209bff73e4030b16
RANTU SAMADDER <2022200000149@seu.edu.bd>
2026-08-09 11:56:36 +0600

    Add initial assembly program structure

---
+++ CW/General Format.asm
@@ -0,0 +1,15 @@
+.model small ;     1 DS + 1 CS
+.stack 100h;   256 bytes
+.data  
+.code
+
+main proc
+    mov ax, @data
+    mov ds, ax ;   DS Activate  
+      
+      
+    exit :  ;   segment build
+    mov ah, 4ch ;  ah=4c
+    int 21h;       interreupt
+    main endp    ; end of procedure
+end main  ; whole program exit
